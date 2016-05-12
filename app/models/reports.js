40855de0bf4faf965d527d249d1c31cc9adc7b19
Пронин Антон <pronin@hazpro.ru>
2016-05-12 09:05:29 +0500

    add report dayli

--- app/models/reports.js
+++ app/models/reports.js
@@ -21,7 +21,57 @@ var defaultReport = db.Schema({
     recall: Boolean, // Повторное обращение
 })
 
+var zakazDayReportAuthor = db.schema({
+    day: Number,
+    month: Number,
+    year: Number,
+    count: Number,
+    cost: Number,
+    author: user.Schema
+
+})
+
+var zakazDayReportSalon = db.schema({
+    day: Number,
+    month: Number,
+    year: Number,
+    count: Number,
+    cost: Number,
+    code: Number
+
+})
+
+var zakazDayReportCity = db.schema({
+    day: Number,
+    month: Number,
+    year: Number,
+    count: Number,
+    cost: Number,
+    City: String
+
+})
+var zdrc = db.model('zakaz_dayli_city', zakazDayReportCity);
+var zdra = db.model('zakaz_dayli_author', zakazDayReportAuthor);
+var zdrs = db.model('zakaz_dayli_salon', zakazDayReportSalon);
+
+function updateReportDayli(data) {
+    if (data.zakaz) {
+        var d = new Date();
+        zdra.findOne({ day: d.getDay(), month: d.getMonth(), year: d.getFullYear, "author._id": data.author._id }, function (err, report) {
+            if (!report) {
+                report.count += 1;
+                report.cost += data.cost;
+                return report.save();
+            }
+            var a = new zdra({ day: d.getDay(), month: d.getMonth(), year: d.getFullYear, author: data.author, count: 1, cost: data.cost })
+            return a.save();
+        })
+    }
+}
 
 module.export = {
-    defualt: db.model('default', defaultReport)
+    defualt: db.model('default', defaultReport),
+    zakaz: {
+        day: updateReportDayli(data)
+    }
 }
