cb7bb69136056105ead3a55ff028b304e336a4f4
nightw0rk <pronin@lorena-kuhni.ru>
2016-05-18 16:35:02 +0500

    add CORS

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ var dns = require('dns');
 var json_parser = require("express-json");
 var bodyParser = require("body-parser");
 var config = require('./config');
+var cors =require("cors");
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
 app.use(bodyParser.json())
+app.use(cors())
 
 dns.resolve4(process.env.MONGO_HOST||'127.0.0.1',function(err,address){
   if(address){
